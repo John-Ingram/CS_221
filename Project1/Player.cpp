a632d4e0b0b0a9f2ea7a526897f5d1b8d8c7db3f
John-Ingram <john@pop-os.localdomain>
2020-09-05 21:35:47 -0500

    Finished project 1

--- Project1/Player.cpp
+++ Project1/Player.cpp
@@ -1,7 +1,9 @@
+// John Ingram|September 5 2020|CS 221
+// This Program Creates a Player object to handle data about baseball players. The file also contains a small test program.
+// Future versions of this Program will not contain the test program, as it will be moved.
+// This program was compiled using gcc version 9.3.0 (Ubuntu 9.3.0-10ubuntu2) using default compiler flags.
 
 #include <iostream>
-#include <fstream>
-#include <iomanip>
 #include <string>
 
 using namespace std;
@@ -13,20 +15,6 @@ private:
     string firstname, lastname;
     int statistics [8]; //integer statistics in this order: plateappearances atbats singles doubles triples homeruns walks hitbypitch
 
-    // grab everything up to and including 'delim' from 'data' and put it in a string 
-    // will ask about this in class, because it seems like a pretty slow way to do this
-    string grabString(istream &data, char delim)
-    {
-        string result = "";
-        char character;
-        while (true)
-        {
-            data >> character;
-            if (character == delim) break;
-            result += character;
-        }
-        return result;
-    }
 
 public:
     // Default constructor
@@ -40,33 +28,112 @@ public:
             this->statistics[0] = 0;
         }
     }
-
-    void grabData(istream &data)
+    // Set Data about the player from 'data'
+    // expected input format is: firstname lastname plateappearances atbats singles doubles triples homeruns walks hitbypitch
+    void setData(istream &data) 
     {
-        // grab names
-        this->firstname = grabString(data, ' ');
-        this->lastname = grabString(data, ' ');
-        // grab other data
+        data >> this->firstname;
+        data >> this->lastname;
         for (int i = 0; i < 8; i++)
         {
             data >> this->statistics[i];
         }
-        cout << "\n" << firstname << " " << lastname;
-        for (int i = 0; i < 8; i++)
-        {
-            cout << "\n" << statistics[i];
-        }
-        
+    }
+    // getters for player data. These will all be very similar
+    string getFirstName()
+    {
+        return this->firstname;
     }
     
+    string getLastName()
+    {
+        return this->lastname;
+    }
+
+    int getPlateAppearances()
+    {
+        return this->statistics[0];
+    }
+    
+    int getAtBats()
+    {
+        return this->statistics[1];
+    }
+
+    int getSingles()
+    {
+        return this->statistics[2];
+    }
+
+    int getDoubles()
+    {
+        return this->statistics[3];
+    }
+
+    int getTriples()
+    {
+        return this->statistics[4];
+    }
+
+    int getHomeRuns()
+    {
+        return this->statistics[5];
+    }
+
+    int getWalks()
+    {
+        return this->statistics[6];
+    }
+
+    int getHitByPitch()
+    {
+        return this->statistics[7];
+    }
+
+    // This method is used by both getBattingAverage and getOnBasePercentage,
+    // It does not technically need to be public, but I figured that it may be useful to a user.
+    int getHits()
+    {
+        return this->getSingles() + this->getDoubles() + this->getTriples() + this->getHomeRuns();
+    }
+    // Calculate batting average
+    float getBattingAverage()
+    {
+        return (float) this->getHits()/(float) this->getAtBats();
+    }
+
+    float getOnBasePercentage()
+    {
+        return (float)(this->getHits()+this->getWalks()+this->getHitByPitch())/(float) this->getPlateAppearances();
+    }
 
 };
 
 int main(int argc, char const *argv[])
 {
-    Player myPlayer;
-    myPlayer.grabData(cin);
+    // Welcome text
+    cout << "Welcome to the test driver program for the Player class.\n";
+    cout << "When prompted enter the players data in the form of\n";
+    cout << "firstname lastname plateappearances atbats singles doubles triples homeruns walks hitbypitch\n";
 
+    //Start infinate loop to repeat test if desired
+    while (true)
+    {
+        Player myPlayer;
+        char restart = 'n';
+        // Prompt
+        cout << "Please enter your data, and confirm with enter: ";
+        myPlayer.setData(cin);
+        // Result output 
+        // firstname and lastname have to be converted to a pointer to the char array, becuse  printf's %s expects a char array
+        printf("\n\t%s %s's Batting Average = %.3f and OBP = %.3f\n\n", myPlayer.getFirstName().c_str(), myPlayer.getLastName().c_str(), myPlayer.getBattingAverage(), myPlayer.getOnBasePercentage());
+
+        // Ask if the user wants to test again
+        cout << "Do you wish to test another [y/n]?";
+        cin >> restart;
+        if (towlower(restart) != 'y') break; // If the user enters anything other than y, break out of the infinite loop to end the program
+        
+    }
     return 0;
 }
 
